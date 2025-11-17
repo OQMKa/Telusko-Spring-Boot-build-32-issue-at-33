04a828ebe53436d6fcb4c15737af670508ef10b9
OQMKa <omkarnake16@gmail.com>
2025-11-17 19:15:00 +0530

    COMMIT - 3.1 : added the @Service and @Repository annotations

--- src/main/java/com/security32/practise/repository/UserRepo.java
+++ src/main/java/com/security32/practise/repository/UserRepo.java
@@ -1,9 +1,10 @@
 package com.security32.practise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.security32.practise.model.Users;
-
+@Repository
 public interface UserRepo extends JpaRepository<Users, Integer>{
 	
 }
